151f9797dfe3fd301c8e3f9dca322c6b6a93e667
isurucuma <isurucuma@gmail.com>
2025-07-04 01:05:48 +0530

    milestone 1 done

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/isurucuma/reverseproxy/internal/proxy"
+	"log/slog"
+)
+
+func main() {
+	listenAddr := flag.String("listen", ":8080", "listen address")
+	backendAddr := flag.String("backend", ":8081", "forward address")
+	flag.Parse()
+
+	slog.Info(fmt.Sprintf("starting proxy server on %s, forwarding to %s", *listenAddr, *backendAddr))
+
+	if err := proxy.Run(*listenAddr, *backendAddr); err != nil {
+		slog.Error("failed to start proxy server", "error", err)
+		return
+	}
+}
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,1 +1,43 @@
 package proxy
+
+import (
+	"io"
+	"log/slog"
+	"net"
+)
+
+func Run(listenAddr, backendAddr string) error {
+	// create a new TCP server
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+	for {
+		// accept a new connection
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Error("failed to accept connection", "error", err)
+		}
+		go handle(conn, backendAddr)
+	}
+}
+
+func handle(conn net.Conn, backendAddr string) {
+	defer conn.Close()
+
+	// create a new connection to the backend server
+	backendConn, err := net.Dial("tcp", backendAddr)
+	if err != nil {
+		slog.Error("failed to connect to backend server", "error", err)
+		return
+	}
+
+	defer backendConn.Close()
+
+	// start a goroutine to copy data from the client to the backend server
+	go io.Copy(backendConn, conn)
+	// copy data from the backend server to the client
+	io.Copy(conn, backendConn)
+}
